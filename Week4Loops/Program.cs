b9426e70ac6bd3d918e45391013240cdc6f98519
MaarikaK <testmaarika@gmail.com>
2023-11-11 15:54:51 +0200

    Week 4

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Random random = new Random();
+
+int myrandom;
+
+
+for (int i = 0; i < 90; i++)
+{
+    myrandom = random.Next(1, 11);
+    Console.WriteLine($"{i + 1}. {myrandom}");
+}
+
+
+
+
+
+
+//for(int i = 0; i < 4; i++) // i++==> i + 1
+//{
+//    Console.WriteLine($"{i + 1}. I will not skip Zoom Class.");
+//}
+
+
---
+++ Week4whileLoop/Week4whileLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Random rnd = new Random();
+int cpuRandom = rnd.Next(1, 4);
+
+bool loopActive = true;
+
+int numberoftries = 0;
+
+while (numberoftries < 3)
+{
+    Console.WriteLine("Try to guess the cpu number:");
+    int userGuess = Convert.ToInt32(Console.ReadLine());
+    numberoftries++;
+
+    if(userGuess == cpuRandom )
+    { 
+        Console.WriteLine("You won!");
+        break;
+    }
+
+
+   
+}
+
+Console.WriteLine($"The number of tries: {numberoftries}");
+Console.WriteLine("Have a nice day!");
